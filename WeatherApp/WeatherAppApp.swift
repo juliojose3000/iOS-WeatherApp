114d4a386661e0eda7f6632b08ee9b079efba43c
juliojose3000 <julidev9810@gmail.com>
2025-08-06 10:36:39 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Julio on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Julio on 6/8/25.
+//
+
+import Testing
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
